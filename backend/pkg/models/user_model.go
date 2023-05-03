45f8cf267d677341736d0a1f114e4c77f1d07547
OoThan <mgoothan.psnl@gmail.com>
2023-05-03 17:11:57 +0630

    add backend models

--- backend/pkg/models/user_model.go
+++ backend/pkg/models/user_model.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	ID                uint64 `gorm:"column:id;primaryKey" json:"id"`
+	Username          string `gorm:"column:username;type:varchar(100);unique;not null" json:"username"`
+	UserNRC           string `gorm:"column:user_nrc;unique;not null" json:"user_nrc"`
+	UserPhoneNumber   string `gorm:"column:user_phone_number;not null" json:"user_phone_number"`
+	ReferenceUserName string `gorm:"column:reference_user_name" json:"reference_user_name"`
+	Street            string `gorm:"column:street;not null"json:"street"`
+	City              string `gorm:"column:city;not null" json:"city"`
+	Region            string `gorm:"column:region;not null" json:"region"`
+
+	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-"`
+}
+
+func (m *User) TableName() string {
+	return "users"
+}
