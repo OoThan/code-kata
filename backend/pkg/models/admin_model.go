abf628494eb127927333933030f0b8b4be1f03aa
OoThan <mgoothan.psnl@gmail.com>
2023-05-03 17:05:21 +0630

    add backend models

---
+++ backend/pkg/models/admin_model.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Admin struct {
+	Id           uint64         `gorm:"column:id;primaryKey" json:"id"`
+	Username     string         `gorm:"column:username;unique;not null" json:"username"`
+	Email        string         `gorm:"column:email;unique;not null" json:"email"`
+	Status       int            `gorm:"column:status" json:"status"`
+	Password     string         `gorm:"column:password" json:"-"`
+	IP           string         `gorm:"column:ip;type:varchar(20)" json:"ip"`
+	Activated2fa bool           `gorm:"column:activated_2fa;default:false;not null" json:"activated_2fa"`
+	SecretKey2fa string         `gorm:"column:secret_key_2fa;size:191" json:"secret_key_2fa"`
+	CreatedAt    time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt    time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `json:"-"`
+}
+
+func (m *Admin) TableName() string {
+	return "admins"
+}
+
+type AdminLog struct {
+	Id        uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	AdminId   uint64         `gorm:"column:admin_id"`
+	Username  string         `gorm:"column:username" json:"username"`
+	LoginUrl  string         `gorm:"column:login_url" json:"login_url"`
+	IP        string         `gorm:"column:ip" json:"ip"`
+	Location  string         `gorm:"column:location;type:varchar(255)" json:"location"`
+	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-"`
+}
+
+func (m *AdminLog) TableName() string {
+	return "admin_logs"
+}
---
+++ backend/pkg/models/loan_package_model.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type LoanPackage struct {
+	Id        uint64         `gorm:"column:id;primaryKey" json:"id"`
+	PackageNo string         `gorm:"column:package_no" json:"package_no"`
+	Creator   uint64         `gorm:"column:creator;not null" json:"creator"`
+	Amount    float64        `gorm:"column:amount" json:"amount"`
+	Percent   float64        `gorm:"column:percent" json:"percent"`
+	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-"`
+}
+
+func (m *LoanPackage) TableName() string {
+	return "loan_packages"
+}
+
+type LoanPackageLog struct {
+	Id        uint64 `gorm:"column:id;primaryKey" json:"id"`
+	PackageNo string `gorm:"column:package_no" json:"package_no"`
+	Creator   uint64 `gorm:"column:creator;not null" json:"creator"`
+
+	BeforeAmount float64 `gorm:"column:before_amount" json:"before_amount"`
+	AfterAmount  float64 `gorm:"column:after_amount" json:"after_amount"`
+
+	BeforePercent float64 `gorm:"column:before_percent" json:"before_percent"`
+	AfterPercent  float64 `gorm:"column:after_percent" json:"after_percent"`
+
+	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-"`
+}
+
+func (m *LoanPackageLog) TableName() string {
+	return "loan_package_logs"
+}
